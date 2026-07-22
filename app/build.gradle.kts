e38acc970520c5a467d4fffa395e1f0f9445c5dd
nift4 <nift4@protonmail.com>
2026-07-22 11:48:27 +0200

    Tried to fix another crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -330,7 +330,7 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
     implementation("androidx.mediarouter:mediarouter:1.8.1")
-    implementation("io.github.nift4.mediastorecompat:mediastorecompat:1.0.0-alpha32")
+    implementation("io.github.nift4.mediastorecompat:mediastorecompat:1.0.0-alpha33")
     val media3Version = "1.10.1"
     implementation("androidx.media3:media3-common-ktx:$media3Version")
     implementation("androidx.media3:media3-exoplayer:$media3Version")
